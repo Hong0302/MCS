f28bf0aca871a606236d42f1c28df3f3a3a3025d
Hong0302 <myth5564@gmail.com>
2018-11-29 15:13:06 +0800

    05050652

--- mcsio.py
+++ mcsio.py
@@ -46,7 +46,6 @@ def post_to_mcs(payload):
 	data = response.read() 
 	conn.close() 
 
-# Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
@@ -57,40 +56,21 @@ else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
-    time.sleep(10)
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 while 1:
-	h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
-
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-
-
-	if h0 is not None and t0 is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-	
+	humidity, temperature= Adafruit_DHT.read_retry(sensor, pin)
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+	SwitchStatus = GPIO.input(24)
+	if humidity is not None and temperature is not None:
+		if (SwitchStatus == 0):
+			print('Button pressed')
+		else:
+			print('Button released')
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-			   {"dataChnId":"Temperature","values":{"value":t0}},{"dataChnID":"SwitchStatus","values":{"value":SwitchStatus}}]} 
+		{"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
 		post_to_mcs(payload)
-		 
-	
-
-
+		time.sleep(1)
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
-		time.sleep(10)
-
-while 1:
-	
-	SwitchStatus = GPIO.input(24)
-	if(SwitchStatus == 0):
-		print('Button pressed')
-	else:
-		print('Button released')
+		time.sleep(1)
